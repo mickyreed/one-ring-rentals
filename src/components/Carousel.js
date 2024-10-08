773a18a6ad9193eec6833777134df4795f76eda5
Micky Reed <xcal18r@gmail.com>
2024-10-08 12:06:20 +0800

    feat: added all css, fonts, images and js folder & contents from base project file. Updated inline style props in Carousel.js to suit React KVP rather than string

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -18,17 +18,84 @@ import React from 'react';
 
 const Carousel = () => {
     return (
-        <section className="carousel">
-            <div className="carouse-item">
-                <img className='carousel-image' src='https://placehold.co/600x400' alt="placeholder-image"/>
-                <div className="carousel-text">
-                    <h1 className="title">Title</h1>
-                    <p className="subtitle">Title</p>
+        <div
+            id="carousel-example-generic"
+            className="carousel slide"
+            data-ride="carousel"
+        >
+            {/*// <!-- Indicators */}
+            {/*//   <ol class="carousel-indicators">*/}
+            {/*//     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>*/}
+            {/*//     <li data-target="#carousel-example-generic" data-slide-to="1"></li>*/}
+            {/*//   </ol> -->*/}
+
+            {/*// <!-- Wrapper for slides -->*/}
+            <div className="carousel-inner" role="listbox">
+                <div
+                    className="item active"
+                    id="slide1"
+                    style={{
+                            backgroundImage: "url(http://placehold.it/1920x605)",
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "left center",
+                            backgroundSize: "cover"
+                        }}
+                >
+                    {/*// <!-- Ready for JS Injection -->*/}
+                    <div className="carousel-caption">
+                        <div className="caption sfr slider-title">Breathtaking views</div>
+                        <div className="caption sfl slider-subtitle">
+                            Relaxation in the Bay of Belfalas
+                        </div>
+                        <a href="#" className="caption sfb btn btn-default btn-lg"
+                        >Learn More</a
+                        >
+                    </div>
+                </div>
+                <div
+                    className="item"
+                    id="slide2"
+                    style={{
+                        backgroundImage: "url(http://placehold.it/1920x605)",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "left center",
+                        backgroundSize: "cover"
+                    }}
+                >
+                    <div className="carousel-caption">
+                        <div className="caption sfr slider-title">The simple life</div>
+                        <div className="caption sfl slider-subtitle">
+                            Lush gardens in Mordor
+                        </div>
+                        <a href="#" className="caption sfb btn btn-default btn-lg"
+                        >Learn More</a
+                        >
+                    </div>
                 </div>
             </div>
-            <button>Button L</button>
-            <button>Button R</button>
-        </section>
+            {/*// <!-- Blue Filter -->*/}
+            <div id="home-search-section"></div>
+
+            {/*// <!-- Controls -->*/}
+            <a
+                className="left carousel-control"
+                href="#carousel-example-generic"
+                role="button"
+                data-slide="prev"
+            >
+                <span className="glyphicon glyphicon-chevron-left"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a
+                className="right carousel-control"
+                href="#carousel-example-generic"
+                role="button"
+                data-slide="next"
+            >
+                <span className="glyphicon glyphicon-chevron-right"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
     );
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,130 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="content has-text-centered">
-                <p>
-                    <strong>One Ring Rentals </strong> | <a href="#">Copyright</a> Michael Reed 20056066.
-                </p>
+        <footer id="footer">
+            <div id="footer-top" className="container">
+                <div className="row">
+                    <div className="block col-sm-3">
+                        <a href="#"
+                        ><img src="images/logo.png" alt="One Ring Rentals"
+                        /></a>
+                        <br/><br/>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                            commodo eros nibh, et dictum elit tincidunt eget. Pellentesque
+                            volutpat quam dignissim, convallis elit id, efficitur sem.
+                            Vivamus ac scelerisque sem. Aliquam sed enim rutrum nibh gravida
+                            pellentesque nec at metus.
+                        </p>
+                    </div>
+                    <div className="block col-sm-3">
+                        <h3>Helpful Links</h3>
+                        <ul className="footer-links">
+                            <li><a href="#">All rentals</a></li>
+                            <li><a href="#">List your rental</a></li>
+                            <li><a href="#">Read our FAQs</a></li>
+                        </ul>
+                    </div>
+                    <div className="block col-sm-6">
+                        <h3>Popular regions</h3>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <ul className="footer-listings">
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p><a href="properties-detail.html">Rhovanion</a></p>
+                                    </li>
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p><a href="properties-detail.html">Eriador</a></p>
+                                    </li>
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p>
+                                            <a href="properties-detail.html">Bay of Belfalas</a>
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-sm-6">
+                                <ul className="footer-listings">
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p><a href="properties-detail.html">Mordor</a></p>
+                                    </li>
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p><a href="properties-detail.html">Arnor</a></p>
+                                    </li>
+                                    <li>
+                                        <div className="image">
+                                            <a href="properties-detail.html"
+                                            ><img src="http://placehold.it/760x670" alt=""
+                                            /></a>
+                                        </div>
+                                        <p><a href="properties-detail.html">Forlindon</a></p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            {/*// <!-- BEGIN COPYRIGHT -->*/}
+            <div id="copyright">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            &copy; 2014 One Ring Rentals
+
+                            {/*// <!-- BEGIN SOCIAL NETWORKS -->*/}
+                            <ul className="social-networks">
+                                <li>
+                                    <a href="#"><i className="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i className="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i className="fa fa-google"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i className="fa fa-pinterest"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i className="fa fa-youtube"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i className="fa fa-rss"></i></a>
+                                </li>
+                            </ul>
+                            {/*// <!-- END SOCIAL NETWORKS -->*/}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*// <!-- END COPYRIGHT -->*/}
         </footer>
     );
 };
